2d7ae14b3db75aae2ab56a5affda53fb692e0599
Nazia Sayyad <sayyadnazia257@gmai.com>
2022-12-14 21:50:51 +0530

    products Api done

---
+++ project/pages/api/product/makeup.js
@@ -0,0 +1,16 @@
+
+import { Mongoconnect } from "../../../database/dbconnect";
+import makeupModel from "../../models/makeup.model";
+
+const Makeup = async (req, res) =>{
+    
+    await Mongoconnect()
+    // lets create products 
+    const {image,small_img,look,title,name,rating,price} = req.body;
+    const prdct =  new makeupModel({image,small_img,look,title,name,rating,price});
+    await prdct.save();
+    // console.log("Product");
+    return res.status(201).send("Product Created Sucessfully");
+
+}
+export default Makeup;
---
+++ project/pages/api/shop/skincare.js
@@ -0,0 +1,5 @@
+
+
+export default async  function skin_product(req,res){
+    res.send("Skincare products Api ")
+}
