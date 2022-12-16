8a3f668254cb129f048b5088d78f365285a9aa8f
Nazia Sayyad <sayyadnazia257@gmai.com>
2022-12-16 13:50:39 +0530

    commit done

--- project/pages/index.js
+++ project/pages/index.js
@@ -1,19 +1,11 @@
+
+import { Cart } from './Products/Cart'
+import { MakeupProducts } from './Products/MakeupProducts'
 export default function Home() {
   return (
    <>
-    {/* <h1> Landing page </h1> */}
-    </>
-import { ChakraProvider } from '@chakra-ui/react'
-import { Cart } from './Products/Cart'
-import { MakeupProducts } from './Products/MakeupProducts'
-
-export default function Home() {
-  return (
-    <div>
-      <ChakraProvider>
-        {/* <MakeupProducts /> */}
-        <Cart/>
-      </ChakraProvider>
-    </div>
+   
+   </>
+   
   )
 }
