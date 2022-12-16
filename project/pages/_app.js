9731c58b10febb51bd8ca1351df69f80f68c2803
Nazia Sayyad <sayyadnazia257@gmai.com>
2022-12-16 16:57:15 +0530

    Landing Page done

--- project/pages/_app.js
+++ project/pages/_app.js
@@ -2,6 +2,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../styles/globals.css'
 import { ChakraProvider } from "@chakra-ui/react";
+import { Navbar } from "../Components/Navbar";
+import Footer from "../Components/Footer";
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
